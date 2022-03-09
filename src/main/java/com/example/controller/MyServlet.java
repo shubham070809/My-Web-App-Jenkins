9db7a1516b428941f92996a96fc2b196cb61c3e0
ibmcloudmicro18 <shubhamsundriyal20@gmail.com>
2022-03-09 18:15:45 +0530

    1 commit

---
+++ src/main/java/com/example/controller/MyServlet.java
@@ -0,0 +1,27 @@
+package com.example.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MyServlet", value = "/MyServlet")
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request,response);
+    }
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter out=response.getWriter();
+        response.setContentType("text/html");
+        out.println("hello world servlet in jenkins");
+        out.println("how are you");
+    }
+}
